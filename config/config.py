3431aae639e67738775005bb392339add4162fe1
MISH0009 <mishuxd.19@gmail.com>
2024-09-17 13:20:23 +0530

    Update config.py
    Signed-off-by: MISH0009 <mishuxd.19@gmail.com>

--- config/config.py
+++ config/config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/",
+    "https://t.me/Dns_Official_Channel",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
