360155c7bbf98eacbc32d38cf6eabf333e913532
MISH0009 <mishuxd.19@gmail.com>
2024-10-06 18:42:24 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/MISH0009/Extra-Plugin",
+    "https://github.com/MISH0009/Extra-plugins",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
