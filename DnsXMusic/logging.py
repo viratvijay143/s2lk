1728343497a338c3668df165cb5f9e3ca262f804
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-14 12:10:59 +0530

    Update logging.py

--- DnsXMusic/logging.py
+++ DnsXMusic/logging.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2024 by MISH0009@Github, < https://github.com/MISH0009 >.
+#
+# This file is part of < https://github.com/MISH0009/DNS > project,
+# and is released under the MIT License.
+# Please see < https://github.com/MISH0009/DNS/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import logging
 from logging.handlers import RotatingFileHandler
 
